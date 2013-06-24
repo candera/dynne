324e1dad17ef1be81e7f3926d62c8290d9dd0693
Craig Andera <candera@wangdera.com>
2013-06-24 07:46:09 -0400

    Update version number to 0.3.0-SNAPSHOT.
    - Also move to latest Incanter.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.craigandera/dynne "0.2.0"
+(defproject org.craigandera/dynne "0.3.0-SNAPSHOT"
   :description "A library for working with audio"
   :url "https://github.com/candera/dynne"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [incanter "1.4.0"]
+                 [incanter "1.5.1"]
                  [com.googlecode.soundlibs/mp3spi "1.9.5-1"]
                  [primitive-math "0.1.2"]]
   :java-source-paths ["src/java"]
