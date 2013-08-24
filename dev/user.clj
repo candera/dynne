ae39e81935f340ccf428acff34db65987f75911e
Craig Andera <candera@wangdera.com>
2013-08-23 22:37:42 -0400

    Refer dynne.sampled-sound in user and not dynne.sound.

--- dev/user.clj
+++ dev/user.clj
@@ -6,8 +6,8 @@
             [clojure.repl :refer [doc pst]]
             [clojure.tools.namespace.repl :refer [refresh]]
             [dynne.operations :as ops]
-            [dynne.sound :refer :all]
-            [dynne.sampled-sound :as samp]
+            ;;[dynne.sound :refer :all]
+            [dynne.sampled-sound :refer :all]
             [hiphip.double :as dbl])
   (:import [javax.sound.sampled
             AudioFileFormat$Type
@@ -48,38 +48,38 @@
 
   (reduce + (r/map #(+ 3 %) [0 1 2 3])))
 
-(defn oversample-all
-  "Oversample all of s, for performance measurement purposes."
-  [s]
-  (let [inc-t (/ 1.0 44100)
-        delta-t (/ inc-t 4)
-        max-t (duration s)
-        channels (channels s)]
-    (loop [t 0.0]
-      (when (< t max-t)
-        (loop [c 0]
-          ;;(sample s t)
-          (when (< c channels)
-            (oversample4 s t c delta-t)
-            (recur (+ 1 c))))
-        (recur (+ t inc-t))))))
+(comment  (defn oversample-all
+            "Oversample all of s, for performance measurement purposes."
+            [s]
+            (let [inc-t (/ 1.0 44100)
+                  delta-t (/ inc-t 4)
+                  max-t (duration s)
+                  channels (channels s)]
+              (loop [t 0.0]
+                (when (< t max-t)
+                  (loop [c 0]
+                    ;;(sample s t)
+                    (when (< c channels)
+                      (oversample4 s t c delta-t)
+                      (recur (+ 1 c))))
+                  (recur (+ t inc-t)))))))
 
-(defn bench
-  [dur]
-  (let [path (str  "test" dur ".wav")]
-   (when-not (.exists (io/file path))
-     (save (->stereo (sinusoid dur 440)) path 44100))
-   (-> ;;path
-       ;;read-sound
-    (sinusoid dur 440)
-    ->stereo
-       (fade-in 20)
-       (fade-out 20)
-       (mix (-> (square-wave 60 880)
-                (timeshift (/ dur 2))
-                (->stereo)))
-       ;;(visualize)
-       ;;(save (str "test-out" dur ".wav") 44100)
-       oversample-all
-       time
-       )))
+(comment (defn bench
+           [dur]
+           (let [path (str  "test" dur ".wav")]
+             (when-not (.exists (io/file path))
+               (save (->stereo (sinusoid dur 440)) path 44100))
+             (-> ;;path
+              ;;read-sound
+              (sinusoid dur 440)
+              ->stereo
+              (fade-in 20)
+              (fade-out 20)
+              (mix (-> (square-wave 60 880)
+                       (timeshift (/ dur 2))
+                       (->stereo)))
+              ;;(visualize)
+              ;;(save (str "test-out" dur ".wav") 44100)
+              oversample-all
+              time
+              ))))
