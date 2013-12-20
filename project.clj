fde5eea4855f257918762b982b25bd24cdab2821
Craig Andera <candera@wangdera.com>
2013-12-20 11:33:31 -0500

    Set version number to 0.4.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.craigandera/dynne "0.4.0-SNAPSHOT"
+(defproject org.craigandera/dynne "0.4.0"
   :description "A library for working with audio"
   :url "https://github.com/candera/dynne"
   :license {:name "Eclipse Public License"
