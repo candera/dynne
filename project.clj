2d70fceeb9c2294ac1f9f6bffe613df3be10ac1c
Craig Andera <candera@wangdera.com>
2014-12-20 09:30:35 -0600

    Update dependencies.

--- project.clj
+++ project.clj
@@ -5,16 +5,16 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [incanter "1.5.4"]
+                 [incanter "1.5.5"]
                  [com.googlecode.soundlibs/mp3spi "1.9.5-1"]
-                 [primitive-math "0.1.3"]
-                 [hiphip-aot "0.1.1"]]
+                 [primitive-math "0.1.4"]
+                 [hiphip-aot "0.1.2"]]
   :java-source-paths ["src/java"]
   :source-paths ["src/clj"]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev
              {:source-paths ["dev"]
-              :dependencies [[org.clojure/tools.namespace "0.2.4"]]
+              :dependencies [[org.clojure/tools.namespace "0.2.8"]]
               ;;               :jvm-opts ^:replace ["-Xdebug" "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=9900"]
               :jvm-opts ^:replace []
               }})
