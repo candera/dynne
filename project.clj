ce8b75d933b6f911da10a6e685210fa763f7591c
Craig Andera <candera@wangdera.com>
2014-12-20 13:42:42 -0600

    Prepare for 0.4.2 release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.craigandera/dynne "0.4.1"
+(defproject org.craigandera/dynne "0.4.2"
   :description "A library for working with audio"
   :url "https://github.com/candera/dynne"
   :license {:name "Eclipse Public License"
